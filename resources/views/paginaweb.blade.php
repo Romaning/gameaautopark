ed12919c98c3ff00894784eb08adf4690fd0df15
Roman <romanfranco767@gmail.com>
2019-11-07 17:43:34 -0400

    Descargar Todo

---
+++ resources/views/paginaweb.blade.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PAGINA WEB</title>
+</head>
+<body>
+PAGINA PRINCIPAL
+<div class="flex-center position-ref full-height">
+    @if (Route::has('login'))
+        <div class="top-right links">
+            @auth {{--VER SI ESTÁ AUTHENTICADO--}}
+            <a href="{{ url('/tablero') }}">Home</a>
+            @else {{--EN EL CASO DE NO ESTAR AUTHENTICADO--}}
+            <a href="{{ route('login') }}">Login</a>
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}">Register</a>
+                @endif
+            @endauth
+        </div>
+    @endif
+    <div class="content">
+        <div class="title m-b-md">
+            Laravel
+        </div>
+        <div class="links">
+            <a href="https://laravel.com/docs">Docs</a>
+            <a href="https://laracasts.com">Laracasts</a>
+            <a href="https://laravel-news.com">News</a>
+            <a href="https://blog.laravel.com">Blog</a>
+            <a href="https://nova.laravel.com">Nova</a>
+            <a href="https://forge.laravel.com">Forge</a>
+            <a href="https://vapor.laravel.com">Vapor</a>
+            <a href="https://github.com/laravel/laravel">GitHub</a>
+        </div>
+    </div>
+</div>
+</body>
+</html>
